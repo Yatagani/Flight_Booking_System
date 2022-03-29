848b0c9669d732971b41dbf7c7eeb4b6a875be75
Kostandin-Dervishaj <kostandin.dervishaj@gmail.com>
2022-03-29 18:08:37 +0200

    atlas configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import express from 'express';
+import User from './models/user';
+import './db/mongoose';
 
 const app = express();
 const port = process.env.PORT;
@@ -6,6 +10,16 @@ app.use(express.json());
 
 app.get('/test', async (request, response) => {
   response.send('Hello here');
+});
+
+app.post('/users', async (request, response) => {
+  const user = new User(request.body);
+  try {
+    await user.save();
+    response.status(201).send(user);
+  } catch (e) {
+    response.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
