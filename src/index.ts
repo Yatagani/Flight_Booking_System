046fb18bd0eebcae775d6abf149d3d88f7ce3d7e
Klejdi Jatagani <famkowalsky@gmail.com>
2022-03-30 09:26:52 +0200

    Project re-structure

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import express from 'express';
 import Path from 'path';
 import YAML from 'yamljs';
 import SwaggerUI from 'swagger-ui-express';
-import routes from './routes';
-import User from './models/user';
-import './db/mongoose';
+import routes from './app/routes';
+import User from './app/modules/authentication/user';
+import './app/config/db';
 
 const app = express();
 const port = process.env.PORT;
@@ -15,12 +15,11 @@ app.use(express.json());
 app.use('/api/v1', routes);
 
 // Setup docs
-const docsFilePath = Path.resolve(__dirname, '../docs/openapi.yaml');
+const docsFilePath = Path.resolve(__dirname, './docs/openapi.yaml');
 const jsonDocsFile = YAML.load(docsFilePath);
 
 app.use(
   '/api/swagger',
-  // basicAuth(config.apiDocsUsername, config.apiDocsPassword, true),
   SwaggerUI.serve,
   SwaggerUI.setup(jsonDocsFile),
 );
