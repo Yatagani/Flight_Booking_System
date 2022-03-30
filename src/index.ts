df9afd812426daa5519d9a1c127d78275517a124
Kostandin-Dervishaj <kostandin.dervishaj@gmail.com>
2022-03-30 09:17:07 +0200

    deleted user import

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import Path from 'path';
 import YAML from 'yamljs';
 import SwaggerUI from 'swagger-ui-express';
 import routes from './routes';
-import User from './models/user';
 import './db/mongoose';
 
 const app = express();
@@ -27,16 +26,6 @@ app.use(
 
 app.get('/test', async (request, response) => {
   response.send('Hello here');
-});
-
-app.post('/users', async (request, response) => {
-  const user = new User(request.body);
-  try {
-    await user.save();
-    response.status(201).send(user);
-  } catch (e) {
-    response.status(400).send(e);
-  }
 });
 
 app.listen(port, () => {
