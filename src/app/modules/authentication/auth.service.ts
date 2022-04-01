372c2bd37e3bc284998a6e53d51fd093bbbb5086
Klejdi Jatagani <famkowalsky@gmail.com>
2022-04-01 17:38:32 +0200

    Finished reset password

--- src/app/modules/authentication/auth.service.ts
+++ src/app/modules/authentication/auth.service.ts
@@ -4,6 +4,7 @@ import Crypto from 'crypto';
 import * as validator from './auth.validator';
 import * as dal from './auth.dal';
 import * as helpers from './auth.helpers';
+import { createToken } from '../../config/authentication/jwt';
 
 export interface IRequestBody {
     email: string,
@@ -59,4 +60,128 @@ export const signUp = async ({ requestBody }: INetworkRequest) => {
   return createUser;
 };
 
-export const logIn = 'login';
+export const resendConfirmationEmail = async ({ requestBody }) => {
+  validator.validateResendConfirmationEmailRequest({ input: requestBody });
+
+  const query = {
+    isConfirmed: false,
+    email: requestBody.email.toLowerCase(),
+  };
+  const update = {
+    confirmationToken: Crypto.randomBytes(32).toString('hex'),
+  };
+  const updatedUser = await dal.updateUser({
+    query,
+    content: update,
+  });
+
+  if (!updatedUser) {
+    // throw new NotFound(errors.USER_NOT_FOUND_OR_ACCOUNT_CONFIRMED);
+    throw new Error('User not found or account confirmed');
+  }
+
+  await helpers.sendConfirmationEmail({
+    user: updatedUser,
+    redirectUrl: requestBody.redirectUrl,
+  });
+
+  return updatedUser;
+};
+
+export const confirmAccount = async ({ requestParams }) => {
+  validator.validateConfirmAccountRequest({ input: requestParams });
+
+  const query = {
+    isConfirmed: false,
+    confirmationToken: requestParams.token,
+  };
+  const update = {
+    confirmationToken: Crypto.randomBytes(32).toString('hex'),
+    isConfirmed: true,
+  };
+  const updatedUser = await dal.updateUser({
+    query,
+    content: update,
+  });
+
+  if (!updatedUser) {
+    // throw new NotFound(errors.USER_NOT_FOUND_OR_ACCOUNT_CONFIRMED);
+    throw new Error('User not found or account created');
+  }
+
+  return updatedUser;
+};
+
+export const logIn = async ({ requestBody }) => {
+  validator.validateLogInRequest({ input: requestBody });
+
+  const user = await dal.findUser({ query: { email: requestBody.email.toLowerCase() } });
+  if (!user) {
+    // throw new NotAuthenticated(errors.USER_NOT_FOUND);
+    throw new Error('Invalid credentials user not found');
+  }
+
+  const passwordsMatch = await Bcrypt.compare(requestBody.password, user.password);
+  if (!passwordsMatch) {
+    // throw new NotAuthenticated(errors.INVALID_PASSWORD);
+    throw new Error('Invalid credentials password');
+  }
+
+  if (!user.isConfirmed) {
+    // throw new NotAuthenticated(errors.ACCOUNT_NOT_CONFIRMED);
+    throw new Error('Invalid credentials confirmation');
+  }
+
+  const sessionToken = createToken(user);
+  const userWithToken = {
+    ...user.toJSON(),
+    token: sessionToken,
+  };
+
+  delete userWithToken.password;
+  // delete userWithToken.twoFactorAuth.secret;
+
+  return userWithToken;
+};
+
+export const requestNewPassword = async ({ requestBody }) => {
+  validator.validateResetPasswordRequest({ input: requestBody });
+
+  const confirmationToken = Crypto.randomBytes(32).toString('hex');
+  const query = { email: requestBody.email.toLowerCase() };
+  const update = { confirmationToken };
+  const updatedUser = await dal.updateUser({
+    query,
+    content: update,
+  });
+
+  if (!updatedUser) {
+    // throw new NotFound(errors.USER_NOT_FOUND);
+    throw new Error('User not found');
+  }
+
+  console.log('UPDATED_USER', updatedUser);
+
+  await helpers.sendEmailWithResetPasswordLink({
+    user: updatedUser,
+    redirectUrl: requestBody.redirectUrl,
+  });
+};
+
+export const resetPassword = async ({ requestBody }) => {
+  validator.validatePasswordUpdateRequest({ input: requestBody });
+
+  const salt = await Bcrypt.genSalt();
+  const hashedPassword = await Bcrypt.hash(requestBody.password, salt);
+  const query = { confirmationToken: requestBody.token };
+  const update = { password: hashedPassword };
+  const updatedUser = await dal.updateUser({
+    query,
+    content: update,
+  });
+
+  if (!updatedUser) {
+    // throw new NotFound(errors.USER_NOT_FOUND);
+    throw new Error('User not found');
+  }
+};
