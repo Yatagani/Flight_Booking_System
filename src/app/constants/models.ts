e1e0dd332d942d16bf4cc0dee5e809f23e519082
Leonora Cikaj <cikaj.leonoraa@gmail.com>
2022-04-01 09:26:08 +0200

    Airport routes

---
+++ src/app/constants/models.ts
@@ -0,0 +1,5 @@
+export default {
+  AIRPLANE_MODEL: 'Airplane',
+  AIRPORT_MODEL: 'Airport',
+  FLIGHT_MODEL: 'Flight',
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import healthRouter from './health';
+import airportRouter from './airports';
 
 const router = Router();
 
 router.use(healthRouter);
+router.use(airportRouter);
 
 export default router;
