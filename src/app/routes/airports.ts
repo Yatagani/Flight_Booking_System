744fc4871fb58e6061a5a49b8085813cfc02db32
Leonora Cikaj <cikaj.leonoraa@gmail.com>
2022-04-04 08:41:21 +0200

    minor fixes

--- src/app/routes/airports.ts
+++ src/app/routes/airports.ts
@@ -1,103 +1,32 @@
 import { Router } from 'express';
-// import Airport from '../modules/airport/airport.model';
 import * as controller from '../modules/airport/airport.controller';
 import routes from '../constants/routes';
-// import { AirportType } from '../modules/airport/airport.types';
+import Passport from 'passport';
 
 const router = Router();
 router.route(routes.AIRPORTS).get(
+  Passport.authenticate('jwt', { session: false }),
   controller.getAirports
 )
 
 router.route(routes.AIRPORTS).post(
+  Passport.authenticate('jwt', { session: false }),
   controller.postAirports
 )
 
 router.route(`${routes.AIRPORTS}/:id`).get(
+  Passport.authenticate('jwt', { session: false }),
   controller.getAirportDetails
 )
 
 router.route(`${routes.AIRPORTS}/:id`).delete(
+  Passport.authenticate('jwt', { session: false }),
   controller.deleteAirport
 )
 
 router.route(`${routes.AIRPORTS}/:id`).patch(
+  Passport.authenticate('jwt', { session: false }),
   controller.updateAirport
 )
-
-// router.route(routes.AIRPORTS).get(async (req, res) => {
-//   try {
-//     const airport: AirportType[] = await Airport.find();
-//     res.status(200).send(airport);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
-
-// router.route(routes.AIRPORTS).post(async (req, res) => {
-//   const airportData = new Airport(req.body);
-//   try {
-//     const airport: AirportType = await airportData.save();
-//     res.status(200).send(airport);
-//   } catch (e) {
-//     console.log(e);    
-//   }
-// })
-
-// router.route(`${routes.AIRPORTS}/:id`).get(async (req, res) => {
-//   const _id = req.params.id;
-//   try {
-//     const existingAirport = await Airport.findById(_id);
-//     if (!existingAirport) {
-//       return res.status(404).send();
-//     }
-//     res.status(200).send(existingAirport);
-//   } catch (e) {
-//     res.status(400).send(e)
-//   }
-// })
-
-// router.route(`${routes.AIRPORTS}/:id`).delete(async (req, res) => {
-//   const _id = req.params.id;
-//   try {
-//     const existingAirport = await Airport.findById(_id);
-    
-//     if (!existingAirport) {
-//       return res.status(404).send();
-//     }
-//     await Airport.deleteOne({ _id });
-//     res.status(204).send();
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// })
-
-// router.route(`${routes.AIRPORTS}/:id`).patch(async (req, res) => {
-//   const _id = req.params.id;
-//   const airportData = req.body;
-//   console.log(airportData);
-  
-//   // const updates = Object.keys(airportData);
-//   // const alloweUpdates = ['name', 'address'];
-//   // const isValidOperation = updates.every(update =>alloweUpdates.includes(update));
-//   // if (!isValidOperation) {
-//   //   res.status(400).send()
-//   // }
-//   try {
-//     const update = await Airport.updateOne({_id}, {$set: airportData}, {multi: true});
-//     console.log(update);
-//     const updatedAirport = await Airport.findById(_id);
-//     console.log(updatedAirport);
-//     if (!updatedAirport) {
-//       return res.status(404).send();
-//     }
-//     // updates.forEach((update) => existingAirport[update] = req.body[update]);
-//     // const updatedAirport = await existingAirport.save();
-//     res.status(200).send(updatedAirport);
-//   } catch (e) {
-//     console.log(e);
-//     res.status(400).send(e);
-//   }
-// })
 
 export default router;
